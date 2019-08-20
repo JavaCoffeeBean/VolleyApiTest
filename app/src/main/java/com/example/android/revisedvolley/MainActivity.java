38e5fa57fdb7d315504c810be71c161fb7f460e9
JavaCoffeeBean <JavaCoffeeBean@gmail.com>
2019-08-20 17:18:19 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/revisedvolley/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.android.revisedvolley;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mTextView;
+    private RequestQueue mQueue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextView = findViewById(R.id.text_View_result);
+        Button buttonParse = findViewById(R.id.button_parse);
+
+        mQueue = Volley.newRequestQueue(this);
+
+        buttonParse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                jsonParse();
+            }
+        });
+    }
+
+    private void jsonParse(){
+
+        String url = "http://api.openweathermap.org/data/2.5/weather?zip=77099,us&APPID=79143c2f91de098f1ab2ca576814fc8d";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONObject jsonObject = response.getJSONObject("main");
+
+                            for (int i = 0; i < jsonObject.length(); i++) {
+//                                JSONObject main_Array = jsonObject.getJSONObject();
+
+                                int temp = jsonObject.getInt("temp");
+                                int temp_min = jsonObject.getInt("temp_min");
+                                int temp_max = jsonObject.getInt("temp_max");
+
+                                mTextView.append(temp + ", " + temp_min + ", " + temp_max + "\n\n");
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        mQueue.add(request);
+    }
+
+}
